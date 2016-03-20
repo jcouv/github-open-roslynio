544c9916ea1bca7561d31721cd47895fffaed7e5
Julien Couvreur <julien.couvreur@gmail.com>
2016-03-20 14:34:49 -0700

    Initial version

---
+++ src/roslyn-symbols.js
@@ -0,0 +1,19 @@
+var id = chrome.contextMenus.create(
+{
+    "title": "Lookup on Roslyn.io",
+    "type": "normal",
+    "contexts": ["selection"],
+    "onclick": genericOnClick,
+    "documentUrlPatterns": ["https://github.com/*"]
+});
+
+function genericOnClick(info, tab) {
+    //console.log("Clicked: " + info.selectionText);
+    //console.log("item " + info.menuItemId + " was clicked");
+    //console.log("info: " + JSON.stringify(info));
+    //console.log("tab: " + JSON.stringify(tab));
+    chrome.tabs.create({
+        selected: true,
+        url: "http://source.roslyn.io/#q="+info.selectionText 
+    });
+}
