628c5104b05e94a9144159b1395551443ed2c32b
Julien Couvreur <julien.couvreur@gmail.com>
2016-03-22 22:41:35 -0700

    Following guidelines

--- src/roslyn-symbols.js
+++ src/roslyn-symbols.js
@@ -1,20 +1,23 @@
 
 var id = chrome.contextMenus.create(
 {
+    "id": "roslyn",
     "title": "Lookup on Roslyn.io",
     "type": "normal",
     "contexts": ["selection"],
-    "onclick": genericOnClick,
     "documentUrlPatterns": ["https://github.com/*/roslyn/*"]
 });
+chrome.contextMenus.onClicked.addListener(genericOnClick);
 
 function genericOnClick(info, tab) {
     //console.log("Clicked: " + info.selectionText);
     //console.log("item " + info.menuItemId + " was clicked");
     //console.log("info: " + JSON.stringify(info));
     //console.log("tab: " + JSON.stringify(tab));
-    chrome.tabs.create({
-        selected: true,
-        url: "http://source.roslyn.io/#q=" + info.selectionText
-    });
+    if (info.menuItemId == "roslyn") {
+        chrome.tabs.create({
+            selected: true,
+            url: "http://source.roslyn.io/#q=" + info.selectionText
+        });
+    }
 }
